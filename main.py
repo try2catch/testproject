058cfeef8dc9198be8a2a72516a51844f322ef22
Akhilesh Singh <akhilesh_singh@ql2.com>
2022-02-09 19:02:08 +0530

    variable added

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     name = 'Try2Catch'
+    address = 'India'
     print_hi(f'Please subscribe like & share the video.:{name}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
