764c7b1b876c19289f966a96253c6e20ee8a7209
try2catch <58857036+try2catch@users.noreply.github.com>
2023-03-28 14:17:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     name = 'Try2Catch'
-    address = 'Akhilesh'
+    address = 'Ramesh'
     print_hi(f'Please subscribe like & share the video.:{name}')
